44479ecb1367527aa2d7a166277e1e1c1ae51e96
Epix <git@exz.me>
2015-04-25 19:41:59 +0800

    test tool

---
+++ testrun.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+# Author: Epix
+from twisted.internet import reactor
+from scrapy.crawler import Crawler
+from scrapy import log, signals
+from Ocular.spiders.pixiv import pixiv
+from scrapy.utils.project import get_project_settings
+
+spider = pixiv()
+settings = get_project_settings()
+crawler = Crawler(settings)
+crawler.configure()
+crawler.crawl(spider)
+crawler.start()
+log.start()
+reactor.run()
